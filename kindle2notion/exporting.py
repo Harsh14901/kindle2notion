a41737c79488cecd000e3bcaecc73669954163fa
Harsh Agrawal <harsh.ag14901@gmail.com>
2025-09-02 17:36:39 +0100

    Improve exporting mechanism
    * The database has additional fields to store whether the last upload included BlockQuotes, Location, Highlight Date. We now check that to decide if the page content needs to be refreshed or not
    * Optimize exporting as separate blocks by batching them in quotes of 100, which is the limit of notion API.
    * Page management is completely automatic at the moment, so manual changes are not expected to persist.
    * Add .envrc to work with direnv and uv.lock files for uv

--- kindle2notion/exporting.py
+++ kindle2notion/exporting.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from typing import Dict, List, Tuple
-
+from typing import Dict, List, Optional, Tuple, cast
+from tqdm import tqdm
 import notional
-from notional.blocks import Paragraph, TextObject, Quote
+from notional.blocks import Paragraph, TextObject, Quote, Page
 from notional.query import TextCondition
-from notional.types import Date, ExternalFile, Number, RichText, Title
+from notional.types import Date, ExternalFile, Number, RichText, Title, Checkbox
 from requests import get
 
 # from notional.text import Annotations
@@ -34,26 +34,37 @@ def export_to_notion(
         (
             formatted_clippings,
             last_date,
-        ) = _prepare_aggregated_text_for_one_book(clippings, enable_location, enable_highlight_date)
-        message = _add_book_to_notion(
-            title,
-            author,
-            clippings_count,
-            formatted_clippings,
-            last_date,
-            notion_api_auth_token,
-            notion_database_id,
-            enable_book_cover,
-            separate_blocks,
+        ) = _prepare_aggregated_text_for_one_book(
+            clippings, enable_location, enable_highlight_date
         )
-        if message != "None to add":
-            print("✓", message)
+        try:
+            message = _add_book_to_notion(
+                title,
+                author,
+                clippings_count,
+                formatted_clippings,
+                last_date,
+                notion_api_auth_token,
+                notion_database_id,
+                enable_book_cover,
+                separate_blocks,
+                enable_location,
+                enable_highlight_date,
+            )
+            if message:
+                print("✓", message)
+            else:
+                print("None to add!")
+        except Exception as e:
+            print(f"An error occured in writing: {title} ({author})")
+            raise e
 
 
 def _prepare_aggregated_text_for_one_book(
-        clippings: List, enable_location: bool, enable_highlight_date: bool
+    clippings: List, enable_location: bool, enable_highlight_date: bool
 ) -> Tuple[str, str]:
     # TODO: Special case for books with len(clippings) >= 100 characters. Character limit in a Paragraph block in Notion is 100
+    # TODO: last date should be based on sorted clippings, not just the last one in the list.
     formatted_clippings = []
     for each_clipping in clippings:
         aggregated_text = ""
@@ -80,6 +91,23 @@ def _prepare_aggregated_text_for_one_book(
     return formatted_clippings, last_date
 
 
+def _write_to_page(
+    notion: notional.session.Session,
+    page_block: Page,
+    separate_blocks: bool,
+    formatted_clippings,
+):
+    if separate_blocks:
+        for st in tqdm(range(0, len(formatted_clippings), 100)):
+            page_contents = [
+                Quote[clip.strip()] for clip in formatted_clippings[st : st + 100]
+            ]
+            notion.blocks.children.append(page_block, *page_contents)
+    else:
+        page_content = Paragraph["".join(formatted_clippings)]
+        notion.blocks.children.append(page_block, page_content)
+
+
 def _add_book_to_notion(
     title: str,
     author: str,
@@ -90,99 +118,109 @@ def _add_book_to_notion(
     notion_database_id: str,
     enable_book_cover: bool,
     separate_blocks: bool,
-):
+    enable_location: bool,
+    enable_highlight_date: bool,
+) -> Optional[str]:
     notion = notional.connect(auth=notion_api_auth_token)
-    last_date = datetime.strptime(last_date, "%A, %d %B %Y %I:%M:%S %p")
-
-    # Condition variables
-    title_exists = False
-    current_clippings_count = 0
+    last_date_dt = datetime.strptime(last_date, "%A, %d %B %Y %I:%M:%S %p")
 
     query = (
         notion.databases.query(notion_database_id)
         .filter(property="Title", rich_text=TextCondition(equals=title))
         .limit(1)
     )
-    data = query.first()
-
-    if data:
-        title_exists = True
-        block_id = data.id
-        block = notion.pages.retrieve(block_id)
-        if block["Highlights"] == None:
-            block["Highlights"] = Number[0]
-        elif block["Highlights"] == clippings_count:  # if no change in clippings
-            title_and_author = str(block["Title"]) + " (" + str(block["Author"]) + ")"
-            print(title_and_author)
-            print("-" * len(title_and_author))
-            return "None to add.\n"
+    page_block: Page = cast(Page, query.first())
+    needs_writing: bool = False
 
     title_and_author = title + " (" + str(author) + ")"
     print(title_and_author)
     print("-" * len(title_and_author))
 
-    # Add a new book to the database
-    if not title_exists:
-        new_page = notion.pages.create(
-            parent=notion.databases.retrieve(notion_database_id),
-            properties={
-                "Title": Title[title],
-                "Author": RichText[author],
-                "Highlights": Number[clippings_count],
-                "Last Highlighted": Date[last_date.isoformat()],
-                "Last Synced": Date[datetime.now().isoformat()],
-            },
-            children=[],
+    if page_block:
+        last_highlighted_dt = cast(
+            datetime,
+            page_block.properties["Last Highlighted"].date.start or datetime.min,
         )
-        # page_content = _update_book_with_clippings(formatted_clippings)
+        blockquoted = cast(bool, page_block.properties["Blockquoted"].checkbox)
+        includes_location = cast(
+            bool, page_block.properties["Includes Location"].checkbox
+        )
+        includes_timestamp = cast(
+            bool, page_block.properties["Includes Timestamp"].checkbox
+        )
+        current_highlight_count = page_block.properties["Highlights"].number or 0
 
+        needs_writing = (
+            (
+                last_highlighted_dt.replace(second=0, tzinfo=None)
+                < last_date_dt.replace(second=0, tzinfo=None)
+            )
+            | (includes_location ^ enable_location)
+            | (includes_timestamp ^ enable_highlight_date)
+            | (blockquoted ^ separate_blocks)
+            | (current_highlight_count != len(formatted_clippings))
+        )
 
-        if separate_blocks:
-            for formatted_clipping in formatted_clippings:
-                page_content = Quote[formatted_clipping.strip()]
-                notion.blocks.children.append(new_page, page_content)
-        else:
-            page_content = Paragraph["".join(formatted_clippings)]
-            notion.blocks.children.append(new_page, page_content)
-
-        block_id = new_page.id
-        if enable_book_cover:
-            # Fetch a book cover from Google Books if the cover for the page is not set
-            if new_page.cover is None:
-                result = _get_book_cover_uri(title, author)
-
-            if result is None:
-                # Set the page cover to a placeholder image
-                cover = ExternalFile[NO_COVER_IMG]
-                print(
-                    "× Book cover couldn't be found. "
-                    "Please replace the placeholder image with the original book cover manually."
-                )
-            else:
-                # Set the page cover to that of the book
-                cover = ExternalFile[result]
-                print("✓ Added book cover.")
-
-            notion.pages.set(new_page, cover=cover)
     else:
-        # update a book that already exists in the database
-        page = notion.pages.retrieve(block_id)
-        # page_content = _update_book_with_clippings(formatted_clippings)
-        page_content = Paragraph["".join(formatted_clippings)]
-        notion.blocks.children.append(page, page_content)
-        # TODO: Delete existing page children (or figure out how to find changes to be made by comparing it with local json file.)
-        current_clippings_count = int(float(str(page["Highlights"])))
-        page["Highlights"] = Number[clippings_count]
-        page["Last Highlighted"] = Date[last_date.isoformat()]
-        page["Last Synced"] = Date[datetime.now().isoformat()]
+        needs_writing = True
 
-    # Logging the changes made
-    diff_count = (
-        clippings_count - current_clippings_count
-        if clippings_count > current_clippings_count
-        else clippings_count
+    if not needs_writing:
+        return
+
+    # Clear the contents of the existing page if we are rewriting.
+    if page_block:
+        notion.pages.delete(page_block)
+
+    # Create a brand new page block with the correct properties
+    page_block = notion.pages.create(
+        parent=notion.databases.retrieve(notion_database_id),
+        properties={
+            "Title": Title[title],
+            "Author": RichText[author],
+            "Last Highlighted": Date[last_date_dt.isoformat()],
+            "Blockquoted": Checkbox[separate_blocks],
+            "Includes Location": Checkbox[enable_location],
+            "Includes Timestamp": Checkbox[enable_highlight_date],
+        },
+        children=[],
     )
-    message = str(diff_count) + " notes/highlights added successfully.\n"
+
+    if enable_book_cover:
+        # Fetch a book cover from Google Books if the cover for the page is not set
+        if page_block.cover is None:
+            result = _get_book_cover_uri(title, author)
+
+        if result is None:
+            # Set the page cover to a placeholder image
+            cover = ExternalFile[NO_COVER_IMG]
+            print(
+                "× Book cover couldn't be found. "
+                "Please replace the placeholder image with the original book cover manually."
+            )
+        else:
+            # Set the page cover to that of the book
+            cover = ExternalFile[result]
+            print("✓ Added book cover.")
+
+        notion.pages.set(page_block, cover=cover)
+
+    _write_to_page(
+        notion=notion,
+        page_block=page_block,
+        separate_blocks=separate_blocks,
+        formatted_clippings=formatted_clippings,
+    )
+
+    # Only write this once content has been succesfully written to page
+    notion.pages.update(
+        page_block,
+        **{
+            "Highlights": Number[clippings_count],
+            "Last Synced": Date[datetime.now().isoformat()],
+        },
+    )
+    page_block.properties["Highlights"] = Number[clippings_count]
+    message = str(clippings_count) + " notes/highlights added successfully.\n"
 
     return message
 
